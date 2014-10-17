025f0c7d0306a327740aba94c0c5fe15fc923d37
nikshev <nikshev81@gmail.com>
2014-10-17 12:20:49 +0300

    First and last commit. Project closed :)

---
+++ freecoin/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace freecoin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.webControl1 = new Awesomium.Windows.Forms.WebControl(this.components);
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.webControl2 = new Awesomium.Windows.Forms.WebControl(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 7);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1012, 74);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Settings";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(720, 29);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(140, 22);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "0.00000001";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(684, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Bet";
+            // 
+            // button1
+            // 
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Location = new System.Drawing.Point(893, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(519, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(140, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(470, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Proxy";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(299, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(140, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "z07092014";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "nikshev@i.ua";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox3.Controls.Add(this.tabControl1);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(12, 87);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1012, 415);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Browsers";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(6, 21);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1000, 388);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.webControl1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(992, 359);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Mining";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // webControl1
+            // 
+            this.webControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webControl1.Location = new System.Drawing.Point(3, 4);
+            this.webControl1.Size = new System.Drawing.Size(986, 350);
+            this.webControl1.TabIndex = 1;
+            this.webControl1.LoadingFrameComplete += new Awesomium.Core.FrameEventHandler(this.Awesomium_Windows_Forms_WebControl_LoadingFrameComplete);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.webControl2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(992, 359);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Lottery";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // webControl2
+            // 
+            this.webControl2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webControl2.Location = new System.Drawing.Point(3, 4);
+            this.webControl2.Size = new System.Drawing.Size(986, 350);
+            this.webControl2.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1036, 514);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "FreeCoin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private Awesomium.Windows.Forms.WebControl webControl1;
+        private Awesomium.Windows.Forms.WebControl webControl2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ freecoin/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Awesomium.Core;
+using System.Threading;
+using Awesomium.Core;
+using System.Collections.Generic;
+
+namespace freecoin
+{
+    public partial class Form1 : Form
+    {
+        private bool work_flag=false;
+        private string login = "";
+        private string password = "";
+        private double begin_bet = 0.0;
+        private double bet = 0.0;
+        private string proxy = "";
+        private string message = "";
+        private bool finishedLoading = false;
+        private bool login_ok = false;
+        private int win_count = 0;
+        private int loose_count = 0;
+ 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Start")
+            {
+                this.message += DateTime.Now.ToString() + ": Start!\r\n";
+                if (textBox1.Text != "" && textBox1.Text.Length>2)
+                {
+                    if (textBox2.Text != "" && textBox1.Text.Length > 2)
+                    {
+                        if (textBox4.Text != "" && textBox1.Text.Length > 2)
+                        {
+                            Cursor.Current = Cursors.WaitCursor;
+                            this.login = textBox1.Text;
+                            this.password = textBox2.Text;
+                            this.bet = Double.Parse(textBox4.Text);
+                            this.begin_bet = Double.Parse(textBox4.Text);
+                            this.proxy = textBox3.Text;
+                            if (!this.login_ok)
+                             this.login_ok=this.CheckLogin();
+                            if (this.login_ok)
+                            {
+                                button1.Text = "Stop";
+                                this.work_flag = true;
+                                textBox1.Enabled = false;
+                                textBox2.Enabled = false;
+                                textBox3.Enabled = false;
+                                textBox4.Enabled = false;
+                                Cursor.Current = Cursors.Default;
+                            }
+                            else
+                            {
+                                MessageBox.Show("Error! Can't login!");
+                                this.message += DateTime.Now.ToString() + ": Can't login!\r\n";
+                                button1.Text = "Start";
+                                this.work_flag = false;
+                                textBox1.Enabled = true;
+                                textBox2.Enabled = true;
+                                textBox3.Enabled = true;
+                                textBox4.Enabled = true;
+                                Cursor.Current = Cursors.Default;
+                            }
+                        }
+                        else 
+                            MessageBox.Show("Error! Check the bet!");
+                    }
+                    else
+                        MessageBox.Show("Error! Check the password!");
+                }
+                else
+                    MessageBox.Show("Error! Check the login!");
+            }
+            else
+            {
+                button1.Text = "Start";
+                this.work_flag = false;
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                textBox3.Enabled = true;
+                textBox4.Enabled = true;
+                Cursor.Current = Cursors.Default;
+                this.message += DateTime.Now.ToString() + ": Stop!\r\n";
+                
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.message != "")
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(System.Environment.CurrentDirectory + "\\log.txt", true))
+                {
+                    file.WriteLine(this.message);
+                }
+                this.message = "";
+            }
+
+            if (this.work_flag)
+            {
+                this.work_flag = false;
+                this.Play();
+                this.work_flag = true;
+            }
+        }
+
+        private bool CheckLogin()
+        {
+            bool ret_val = false;
+            this.finishedLoading = false;
+            try
+            {
+                webControl1.Source = new Uri("https://freebitco.in/?op=home");
+
+                while (!this.finishedLoading)
+                {
+                    Thread.Sleep(100);
+                    WebCore.Update();
+                }
+
+                dynamic document = (JSObject)webControl1.ExecuteJavascriptWithResult("document");
+
+                while (true)
+                {
+                    if (document != null && document.ToString().Length > 0)
+                        break;
+
+                    document = (JSObject)webControl1.ExecuteJavascriptWithResult("document");
+                }
+
+                if (document != null)
+                {
+                    using (document)
+                    {
+                        dynamic switch_to_login = document.getElementById("switch_to_login_button");
+                        if (switch_to_login != null)
+                        {
+                            switch_to_login.click();
+
+                            dynamic login_field = document.getElementById("login_form_btc_address");
+                            if (login_field != null)
+                            {
+                                login_field.value = this.login;
+                                dynamic pass_field = document.getElementById("login_form_password");
+                                if (pass_field != null)
+                                {
+                                    pass_field.value = this.password;
+                                    dynamic submit_btn = document.getElementById("login_button");
+                                    if (submit_btn != null)
+                                    {
+                                        submit_btn.click();
+                                        Uri uriPlay = new Uri("https://freebitco.in/?op=home#");
+                                        this.finishedLoading = false;
+                                        webControl1.Source = uriPlay;
+                                        while (!this.finishedLoading)
+                                        {
+                                            Thread.Sleep(100);
+                                            WebCore.Update();
+                                        }
+                                        ret_val = true;
+                                    }
+                                    else
+                                        this.message += DateTime.Now.ToString() + ": Error! Can't find submit button!\r\n"; 
+                                }
+                                else
+                                    this.message += DateTime.Now.ToString() + ": Error! Can't find password field!\r\n";
+                            }
+                            else
+                                this.message += DateTime.Now.ToString() + ": Error! Can't find login field!\r\n";
+                        }
+                        else
+                            this.message += DateTime.Now.ToString() + ": Error! Can't find switch button!\r\n";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                this.message += DateTime.Now.ToString() + ": Error! Exception in CheckLogin:"+ex.Message+"!\r\n";
+            }
+            return ret_val;
+        }
+
+        private void Play()
+        {
+            bool win = false;
+            
+            try
+            {
+                dynamic document = (JSObject)webControl1.ExecuteJavascriptWithResult("document");
+
+                while (true)
+                {
+                    if (document != null && document.ToString().Length > 0)
+                        break;
+
+                    document = (JSObject)webControl1.ExecuteJavascriptWithResult("document");
+                }
+
+                if (document != null)
+                {
+                    using (document)
+                    {
+                        int i = 0;
+                        dynamic balance_field_pre = document.getElementById("balance");
+                        double balance_old = Double.Parse(balance_field_pre.innerText); ;
+                        dynamic btc_stake = document.getElementById("double_your_btc_stake");
+                        if (btc_stake != null)
+                            {
+
+                                btc_stake.value = this.bet.ToString("0.000000000");
+
+                                dynamic submit_btn = document.getElementById("double_your_btc_bet_hi_button");
+                                if (submit_btn != null)
+                                 {
+                                   submit_btn.click();
+                                  
+                                  
+                                    Thread.Sleep(6000);
+                                    WebCore.Update();
+                                    
+                                    dynamic win_bet = document.getElementById("double_your_btc_bet_win");
+                                    dynamic loose_bet = document.getElementById("double_your_btc_bet_lose");
+                                    string win_bet_value = win_bet.innerText;
+                                    string loose_bet_value = loose_bet.innerText;
+                                    dynamic balance_field = document.getElementById("balance");
+                                    double balance = Double.Parse(balance_field.innerText);
+                                                                    
+                                    if ((win_bet_value != null&&win_bet_value.Length>5))
+                                    {
+                                        this.message += DateTime.Now.ToString() + ": Win with bet:" + this.bet.ToString() + "!\r\n";
+                                        this.bet = this.begin_bet;
+                                        win = true;
+                                        balance_old = balance;
+                                        this.win_count++;
+                                    }
+
+                                    if ((loose_bet_value != null && loose_bet_value.Length > 5))
+                                    {
+                                        this.bet *= 2;
+                                      
+                                        this.loose_count++;
+                                        if (this.loose_count > 5)
+                                        {
+                                            this.loose_count = 0;
+                                            webControl1.Reload(true);
+                                        }
+
+                                        this.message += DateTime.Now.ToString() + ": Loose! Last bet:" + this.bet.ToString() + "!\r\n";
+                                        balance_old = balance;
+                                    }
+                                }
+                                else
+                                   this.message += DateTime.Now.ToString() + ": Error! Can't find submit button!\r\n";
+                            }
+                            else
+                                this.message += DateTime.Now.ToString() + ": Error! Can't find btc stake field\r\n";
+                        }
+
+                    }
+            }
+            catch (Exception ex)
+            {
+                this.message += DateTime.Now.ToString() + ": Error! Exception in Play:" + ex.Message + "!\r\n";
+            }
+        }
+
+        private void Awesomium_Windows_Forms_WebControl_LoadingFrameComplete(object sender, Awesomium.Core.FrameEventArgs e)
+        {
+            if (e.IsMainFrame)
+            {
+                this.finishedLoading = true;
+            }
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1_Click(null, null);
+        }
+    }
+}
